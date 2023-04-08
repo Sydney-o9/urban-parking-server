1b1fbf973f321598d7446e09c72e40e8207c471d
Anthony Rey <anthony.rey21@gmail.com>
2023-04-08 20:33:26 +1000

    Test 4

---
+++ app.js
@@ -0,0 +1,25 @@
+// require("dotenv").config();
+// require("../database/index");
+// const express = require("express");
+// const path = require("path");
+// const morgan = require("morgan");
+// const cors = require("cors");
+// const router = require("../routes/index");
+// const app = express();
+
+// app.use(cors());
+// app.use(morgan("dev"));
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname, "public")));
+
+// app.use(router);
+
+// module.exports = app;
+
+const app = require('express')();
+
+app.get('/api', (req, res) => {
+  res.end('Hello Kevin!');
+});
+
+module.exports = app;
